67838cc2e74369f767c696896babdd13fc88c7ba
Chineze Okadigbo <chinezelum.okadigbo@pau.edu.ng>
2021-01-21 14:55:52 +0000

    First commit
    **NOTE**errors to be rectified
    
    packagae.json
    package-lock.json
    tsconfig.json
    
    21st Jan 2021

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var firstName = "Chineze";
+console.log(firstName);
+var count = 10;
+firstName = "Paul";
+console.log(firstName);
+count = 5;
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let firstName: string = "Chineze";
+
+console.log(firstName);
+
+let count: number = 10;
+
+firstName = "Paul";
+
+console.log(firstName);
+
+count = 5;
