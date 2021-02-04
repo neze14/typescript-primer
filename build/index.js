053e8f89f24a9417f3c1cd96c9e06aae8f85ee49
neze14 <72756219+neze14@users.noreply.github.com>
2021-02-04 14:52:05 +0100

    Second commit
    4th February 2021
    
    npx run build(command used to create build folder)
    Student-register project created

---
+++ build/index.js
@@ -0,0 +1,31 @@
+"use strict";
+/*
+let firstName: string = "Chineze";
+
+console.log(firstName);
+
+let count: number = 10;
+
+firstName = "Paul";
+
+console.log(firstName);
+
+count = 5;
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Person = void 0;
+class Person {
+    constructor(firstName, lastName, gender, height, hobbies, birthday, isAlive) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.height = height;
+        this.hobbies = hobbies;
+        this.birthday = birthday;
+        this.isAlive = isAlive;
+    }
+}
+exports.Person = Person;
+;
+console.log(name);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/*
 let firstName: string = "Chineze";
 
 console.log(firstName);
@@ -9,3 +10,41 @@ firstName = "Paul";
 console.log(firstName);
 
 count = 5;
+*/
+
+export class Person{
+    //declare property types
+    firstName: string;
+    lastName: string;
+    gender: string;
+    height: number;
+    hobbies: string[];
+    birthday: Date;
+    isAlive: boolean; 
+
+    constructor(firstName: string, lastName: string, gender: string, height: number, 
+        hobbies: string[], birthday: Date, isAlive: boolean){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.height = height;
+        this.hobbies = hobbies;
+        this.birthday = birthday;
+        this.isAlive = isAlive;
+    }
+};
+
+//Accessing object properties
+const firstName: string = "Pius";
+const lastName:string = "Onobhayedo";
+const gender: string = "male";
+const height: number = 1.7;
+const hobbies: string[] = ['Tennis','Programming'];
+const birthday: Date = new Date(1900,4,5);
+const isAlive: boolean = true;
+
+const person1 = new Person(firstName, lastName, gender, height, 
+    hobbies, birthday, isAlive)
+
+console.log(`Person 1 is ${person1.firstName} whose height is ${person1.height} 
+    and birthday is $ {person1.birthday.toLocaleDateString()} and hobbies are ${person1.hobbies}. `);
